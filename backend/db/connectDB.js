06eed2a431268fabe699aa9d70c25c52ca07ccc4
FebzLeoCena <febinky07@gmail.com>
2024-04-27 15:16:54 +0530

    Project boilerplate setup

---
+++ backend/db/connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+    process.exit(1); //terminate the Node.js process with an exit code of 1
+  }
+};
